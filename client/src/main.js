5fa828c82fa8630a4a5d6160e7ee464296b794ea
Chinmay Sahu <chinmay235@gmail.com>
2026-02-05 14:53:58 +0530

    Initialize React app with Apollo Client

---
+++ client/src/main.js
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import './index.css'
+import App from './App.jsx'
+import client from "./ApolloClientConfig.jsx"
+import { ApolloProvider } from "@apollo/client/react";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </StrictMode>,
+)
